e465d4e7fa62bea620020e52c332d7545d7e94df
kaverivs <vskaveri2016@gmail.com>
2021-08-20 23:15:16 +0530

    Add files via upload

---
+++ Exception Handling/src/exception/handling/JavaExceptionExample2.java
@@ -0,0 +1,9 @@
+package exception.handling;
+//NullPointerException 
+public class JavaExceptionExample2 {
+	public static void main(String[] args) {
+	      //code that may raise exception  
+		String s=null;  
+		System.out.println(s.length());	
+		}
+}
---
+++ Exception Handling/src/exception/trycatch/TryCatchExample2.java
@@ -0,0 +1,19 @@
+package exception.trycatch;
+
+public class TryCatchExample2 {
+
+    public static void main(String[] args) {  
+        try  
+        {  
+        int data=50/0; //may throw exception   
+                         // if exception occurs, the remaining statement will not exceute  
+        System.out.println("rest of the code");  
+        }  
+             // handling the exception   
+        catch(ArithmeticException e)  
+        {  
+            System.out.println(e);  
+        }  
+          
+    }  
+}
