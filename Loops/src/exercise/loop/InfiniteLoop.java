a71a246ebe9a7901f80220b7ee03c20fb228d89e
kaverivs <vskaveri2016@gmail.com>
2021-08-21 00:48:38 +0530

    Delete Loops directory

--- Loops/src/exercise/loop/InfiniteLoop.java
+++
@@ -1,9 +0,0 @@
-package exercise.loop;
-
-public class InfiniteLoop {
-	public static void main(String[] args) {  
-	    while(true){  
-	        System.out.println("infinitive while loop");  
-	    }  
-	}  
-}
--- Loops/src/exercise/loop/InfinitiveDoWhile.java
+++
@@ -1,9 +0,0 @@
-package exercise.loop;
-
-public class InfinitiveDoWhile {
-	public static void main(String[] args) {  
-	    do{  
-	        System.out.println("infinitive do while loop");  
-	    }while(true);  
-	} 
-}
