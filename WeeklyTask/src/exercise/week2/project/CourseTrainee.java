9197d523a8ea2c13b46517ad463cfe9d5a738ab7
kaverivs <vskaveri2016@gmail.com>
2021-08-21 00:49:46 +0530

    Delete WeeklyTask directory

--- WeeklyTask/src/exercise/week2/project/CourseTrainee.java
+++
@@ -1,5 +0,0 @@
-package exercise.week2.project;
-
-public interface CourseTrainee {
-	public int emailChecker();
-}
--- WeeklyTask/src/exercise/week2/project/Main.java
+++
@@ -1,9 +0,0 @@
-package exercise.week2.project;
-
-public class Main {
-	public static void main(String[] args) {
-		
-		Display d=new LoginDisplay();
-		d.display();
-	}
-}
