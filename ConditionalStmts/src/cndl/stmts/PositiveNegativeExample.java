9c1a479fd306be7f3855044ea6a8e28d8db826fd
kaverivs <vskaveri2016@gmail.com>
2021-08-21 00:08:53 +0530

    Add files via upload

---
+++ ConditionalStmts/src/cndl/stmts/PositiveNegativeExample.java
@@ -0,0 +1,16 @@
+package cndl.stmts;
+
+public class PositiveNegativeExample {
+	public static void main(String[] args) {    
+	    int number=-13;    
+	    if(number>0){  
+	    System.out.println("POSITIVE");  
+	    }else if(number<0){  
+	    System.out.println("NEGATIVE");  
+	    }else{  
+	    System.out.println("ZERO");  
+	   }  
+	}    
+
+
+}
